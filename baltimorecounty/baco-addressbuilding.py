83927b95c0f266423f6d36f418f88d14767b2f58
Elliott Plack <elliott.plack@gmail.com>
2014-08-15 13:04:20 -0400

    fix 10 char max
    silly shapefile field names capped at 10 chars

--- baltimorecounty/baco-addressbuilding.py
+++ baltimorecounty/baco-addressbuilding.py
@@ -8,11 +8,13 @@ def filterTags(attrs):
     tags = {}
 
     # Changing tag caps
-    if 'FEATURE_TYPE' in attrs:
+    if 'FEATURE_TY' in attrs:
         tags['building'] = attrs['FEATURE_TYPE']
-    if 'ADDR_HOUSENUMBER' in attrs:
+    if 'BUILDING' in attrs:
+        tags['building'] = attrs['FEATURE_TYPE']
+    if 'ADDR_HOUSE' in attrs:
         tags['addr:housenumber'] = attrs['ADDR_HOUSENUMBER']
-    if 'ADDR_STREET' in attrs:
+    if 'ADDR_STREE' in attrs:
         tags['addr:street'] = attrs['ADDR_STREET']
     if 'ADDR_STATE' in attrs:
         tags['addr:state'] = attrs['ADDR_STATE']
@@ -20,7 +22,7 @@ def filterTags(attrs):
         tags['addr:city'] = attrs['ADDR_CITY']
     if 'POSTCODE' in attrs:
         tags['addr:postcode'] = attrs['POSTCODE']
-    if 'ADDR_POSTAL' in attrs:
+    if 'ADDR_POSTA' in attrs:
         tags['addr:postcode'] = attrs['ADDR_POSTAL']
 
     return tags
