bda4e555169f98882cd04012b8c6cbacb5e2050c
Elliott Plack <elliott.plack@gmail.com>
2014-08-15 13:26:47 -0400

    more squashing

--- baltimorecounty/baco-addressbuilding.py
+++ baltimorecounty/baco-addressbuilding.py
@@ -9,13 +9,13 @@ def filterTags(attrs):
 
     # Changing tag caps
     if 'FEATURE_TY' in attrs:
-        tags['building'] = attrs['FEATURE_TYPE']
+        tags['building'] = attrs['FEATURE_TY']
     if 'BUILDING' in attrs:
         tags['building'] = attrs['BUILDING']
     if 'ADDR_HOUSE' in attrs:
-        tags['addr:housenumber'] = attrs['ADDR_HOUSENUMBER']
+        tags['addr:housenumber'] = attrs['ADDR_HOUSE']
     if 'ADDR_STREE' in attrs:
-        tags['addr:street'] = attrs['ADDR_STREET']
+        tags['addr:street'] = attrs['ADDR_STREE']
     if 'ADDR_STATE' in attrs:
         tags['addr:state'] = attrs['ADDR_STATE']
     if 'ADDR_CITY' in attrs:
@@ -23,6 +23,6 @@ def filterTags(attrs):
     if 'POSTCODE' in attrs:
         tags['addr:postcode'] = attrs['POSTCODE']
     if 'ADDR_POSTA' in attrs:
-        tags['addr:postcode'] = attrs['ADDR_POSTAL']
+        tags['addr:postcode'] = attrs['ADDR_POSTA']
 
     return tags
